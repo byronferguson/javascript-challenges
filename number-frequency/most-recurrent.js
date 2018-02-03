74cbc3a29d532a346db3e106e27b068a89f57d7c
Byron Ferguson <byron.ferguson@homecreditus.com>
2018-02-03 00:40:59 -0600

    number freq solution

--- number-frequency/most-recurrent.js
+++ number-frequency/most-recurrent.js
@@ -1,5 +1,7 @@
 const mostRecurrent = (set) => {
-  return set
+  return set.sort((a,b) => {
+    set.filter(v => v === a).length - set.filter(v => v === b).length
+  }).pop();
 }
 
 module.exports = mostRecurrent
